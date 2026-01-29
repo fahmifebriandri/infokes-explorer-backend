a08b79a14df767da947291d1acee69efd8f302f0
fahmifebriandri <fahmifebriandri@gmail.com>
2026-01-29 18:25:23 +0700

    fix server host

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = Number(process.env.PORT) || 3000;
+
 async function bootstrap() {
-    try {
-        await AppDataSource.initialize();
-        console.log("📦 Database connected");
-    } catch (err) {
-        console.error("❌ Failed to start app", err);
-    }
+  try {
+    await AppDataSource.initialize();
+    console.log("📦 Database connected");
+
+    app.listen(PORT);
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  } catch (err) {
+    console.error("❌ Failed to start app", err);
+    process.exit(1);
+  }
 }
 
 bootstrap();
-
-export default app;
